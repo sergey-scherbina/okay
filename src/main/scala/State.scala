41d72bd4e578d231e96d46f7f6378a4bbfcfbd0a
Sergiy Shcherbyna <sergey.scherbina@gmail.com>
2025-08-16 21:08:59 +0200

    okay 0.1.0

--- src/main/scala/State.scala
+++ src/main/scala/State.scala
@@ -2,6 +2,7 @@ package okay
 
 import scala.annotation.tailrec
 import scala.util.control.TailCalls.*
+import okay.Eff.*
 
 infix type %[F[_, _], S] = F[S, *]
 
@@ -10,37 +11,37 @@ enum State[S, +A] {
   case Put(s: S) extends State[S, S]
 }
 
+extension [A](a: A)
+  def state[S]: A ! State % S = Eff.pure(a)
+
 object State {
-  inline def getState[S]: S ! State % S = effect(Get())
-  inline def putState[S](s: S): S ! State % S = effect(Put(s))
+  inline def get[S]: S ! State % S = effect(Get())
+  inline def set[S](s: S): S ! State % S = effect(Put(s))
 
-  extension [A](a: A)
-    def state[S]: A ! State % S = Eff.pure(a)
-
-  def handleState[S, A, F[+_]](s: S)(a: A ! State % S + F): (S, A) ! F = {
+  def handle[S, A, F[+_]](s: S)(a: A ! State % S + F): (S, A) ! F = {
     def _loop(s: S)(x: A ! State % S + F): (S, A) ! F = loop(s)(x)
 
     @tailrec def loop(s: S)(x: A ! State % S + F): (S, A) ! F = x match
-      case !.Continue(!.Continue(a, h), k) => loop(s)(a.flatMap(h(_).flatMap(k)))
-      case !.Continue(!.Pure(a), k) => loop(s)(k(a))
-      case !.Pure(a) => pure((s, a))
-      case !.Effect(e) => Eff.<|>[State[S, *], F](e) match
-        case Left(Get()) => pure((s, s))
-        case Left(Put(s)) => pure((s, s))
-        case Right(e) => effect(e).map((s, _))
-      case !.Continue(!.Effect(e), k) => Eff.<|>[State[S, *], F](e) match
+      case Continue(Continue(a, h), k) => loop(s)(a.flatMap(h(_).flatMap(k)))
+      case Continue(Pure(a), k) => loop(s)(k(a))
+      case Pure(a) => Pure((s, a))
+      case Effect(e) => <|>[State[S, *], F](e) match
+        case Left(Get()) => Pure((s, s))
+        case Left(Put(s)) => Pure((s, s))
+        case Right(e) => Effect(e).map((s, _))
+      case Continue(Effect(e), k) => <|>[State[S, *], F](e) match
         case Left(Get()) => loop(s)(k(s))
         case Left(Put(s)) => loop(s)(k(s))
-        case Right(e) => effect(e).flatMap(x => _loop(s)(k(x)))
+        case Right(e) => Effect(e).flatMap(x => _loop(s)(k(x)))
 
     loop(s)(a)
   }
 
-  inline def runState[S, A](s: S)(a: A ! State % S): (S, A) =
-    Eff.run(handleState(s)(a))
+  inline def run[S, A](s: S)(a: A ! State % S): (S, A) =
+    Eff.run(handle(s)(a))
 
-  def index[A](seq: Seq[A], begin: Long = 0): (Long, Seq[(Long, A)]) = runState(begin):
+  def index[A](seq: Seq[A], from: Long = 0): (Long, Seq[(Long, A)]) = run(from):
     seq.foldLeft(Seq[(Long, A)]().state[Long]): (c, a) =>
-      for xs <- c; n <- getState; _ <- putState(n + 1) yield (n, a) +: xs
+      for xs <- c; n <- get; _ <- set(n + 1) yield (n, a) +: xs
 
 }
