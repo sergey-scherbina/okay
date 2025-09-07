54a83fb3ae8b394c1cc4ce2de47fb1a918d25b9c
Sergiy Shcherbyna <sergey.scherbina@gmail.com>
2025-09-07 06:08:31 +0200

    Throws, wrap unwrap

--- src/test/scala/TestThrows.scala
+++ src/test/scala/TestThrows.scala
@@ -8,10 +8,7 @@ class TestThrows {
   @Test def testThrows1(): Unit = {
     val x = List[String throws Fault]("a", Right("b"), Fault("c"),
       unsafe(throw Fault("d")), unsafe(s"${0 / 1}"), unsafe(s"${0 / 0}"))
-
     println(x)
-    // List(a, Right(b), TestResult$package$Fault$1: c, TestResult$package$Fault$1: d
-    // , 0, Failure(java.lang.ArithmeticException: / by zero))
   }
 
   @Test def testThrows2(): Unit = {
