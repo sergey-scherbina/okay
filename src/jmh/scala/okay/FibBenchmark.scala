c5ec3c959bee73fb924c09868c940d5b28255a5c
Sergiy Shcherbyna <sergey.scherbina@gmail.com>
2025-10-28 00:00:49 +0100

    benchmark

--- src/jmh/scala/okay/FibBenchmark.scala
+++ src/jmh/scala/okay/FibBenchmark.scala
@@ -2,28 +2,29 @@ package okay
 
 import org.openjdk.jmh.annotations._
 import java.util.concurrent.TimeUnit
+import !.*
 
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.AverageTime))
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Warmup(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 10, time = 2, timeUnit = TimeUnit.SECONDS)
-@Fork(3)
+@Warmup(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(2)
 class FibBenchmark {
 
   @Benchmark
-  def fib10(): List[Int] =
-    fib[Int, LazyList].take(10).toList
+  def fib10(): Any =
+    fib[Int, Producer].next(10).?
 
   @Benchmark
-  def fib50(): List[Int] =
-    fib[Int, LazyList].take(50).toList
+  def fib50(): Any =
+    fib[Int, Producer].next(50).?
 
   @Benchmark
-  def fib100(): List[BigInt] =
-    fib[BigInt, LazyList].take(100).toList
+  def fib100(): Any =
+    fib[BigInt, Producer].next(100).?
 
   @Benchmark
-  def fib1000(): List[BigInt] =
-    fib[BigInt, LazyList].take(1000).toList
+  def fib1000(): Any =
+    fib[BigInt, Producer].next(1000).?
 }
